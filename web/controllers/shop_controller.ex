a2b19774191aae84a8f79e3046ccf3a53d077d50
tracehelms <tracehelms@gmail.com>
2015-09-23 21:11:33 -0600

    add placecage

--- web/controllers/shop_controller.ex
+++ web/controllers/shop_controller.ex
@@ -3,16 +3,16 @@ defmodule ALatteArt.ShopController do
 
   def index(conn, _params) do
     shops = [
-      %{name: "The Laughing Goat", id: 1, location: "123, 456"},
-      %{name: "Pekoe Sip House", id: 2, location: "123, 456"},
-      %{name: "Ozo Coffee", id: 3, location: "123, 456"},
-      %{name: "Boxcar Coffee Roasters", id: 4, location: "123, 456"},
-      %{name: "Red Rock Coffeehouse", id: 5, location: "123, 456"},
-      %{name: "The Cup", id: 6, location: "123, 456"},
-      %{name: "Flatiron Coffee", id: 7, location: "123, 456"},
-      %{name: "Village Coffeeshop", id: 8, location: "123, 456"},
-      %{name: "The Unseen Bean", id: 9, location: "123, 456"},
-      %{name: "Market Brewers Coffee", id: 10, location: "123, 456"}
+      %{name: "The Laughing Goat", id: 1, location: "123, 456", src: "http://www.placecage.com/g/200/200"},
+      %{name: "Pekoe Sip House", id: 2, location: "123, 456", src: "http://www.placecage.com/g/200/200"},
+      %{name: "Ozo Coffee", id: 3, location: "123, 456", src: "http://www.placecage.com/g/200/200"},
+      %{name: "Boxcar Coffee Roasters", id: 4, location: "123, 456", src: "http://www.placecage.com/g/200/200"},
+      %{name: "Red Rock Coffeehouse", id: 5, location: "123, 456", src: "http://www.placecage.com/g/200/200"},
+      %{name: "The Cup", id: 6, location: "123, 456", src: "http://www.placecage.com/g/200/200"},
+      %{name: "Flatiron Coffee", id: 7, location: "123, 456", src: "http://www.placecage.com/g/200/200"},
+      %{name: "Village Coffeeshop", id: 8, location: "123, 456", src: "http://www.placecage.com/g/200/200"},
+      %{name: "The Unseen Bean", id: 9, location: "123, 456", src: "http://www.placecage.com/g/200/200"},
+      %{name: "Market Brewers Coffee", id: 10, location: "123, 456", src: "http://www.placecage.com/g/200/200"}
     ]
     json conn, shops
   end
